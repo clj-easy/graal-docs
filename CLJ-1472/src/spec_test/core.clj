a4d3b5086785a13720937f78938f9ed3b0507867
lread <lee@dlread.com>
2023-07-29 18:41:03 -0400

    Freshen docs
    Turf `./CLJ-1242`. We worked hard on this and should be proud, but it is
    no longer relevant since the Clojure core team has resolved the issue.
    
    Converted remaining docs from .md to .adoc.
    For consistency. And 'cause I like adoc.
    
    Rephrase, reword where it seemed it would help.
    Fix broken links.
    
    Examples now use clj-easy's graal-build-time instead of the now deprecated
    `--initialize-at-build-time`.
    
    README
    - Add section on GraalVM version
    - Move related content under sub-sections
    - Update and add more examples to reflection section
    
    Hello world
    - GRAALVM_HOME seems to be no longer necessary/recommended, simplifying
    our examples.
    - annoy borkdude by mentioning sdkman
    
    Turfed ./resources/*.java - this was for old reflection tips that are no
    longer relevant for current versions of Clojure and GraalVM.
    
    Closes #58

--- CLJ-1472/src/spec_test/core.clj
+++
@@ -1,9 +0,0 @@
-(ns spec-test.core
-  (:gen-class)
-  (:require [clojure.spec.alpha :as s]))
-
-(s/def ::g int?)
-
-(defn -main [& _args]
-  (println *clojure-version*)
-  (println (s/valid? ::g 1)))
